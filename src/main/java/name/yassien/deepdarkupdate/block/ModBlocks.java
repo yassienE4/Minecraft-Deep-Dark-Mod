a42e09c4c3ab98a5a45ef26e5232bb94e17905bc
yassienE4 <yassienebrahim@gmail.com>
2024-12-16 03:35:59 +0200

    started blocks

---
+++ src/main/java/name/yassien/deepdarkupdate/block/ModBlocks.java
@@ -0,0 +1,14 @@
+package name.yassien.deepdarkupdate.block;
+
+import name.yassien.deepdarkupdate.DeepDarkUpdate;
+
+public class ModBlocks
+{
+
+
+
+    public static void registerModBlocks()
+    {
+        DeepDarkUpdate.LOGGER.info("Registering Mod Blocks for " + DeepDarkUpdate.MOD_ID);
+    }
+}
